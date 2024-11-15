a005d3a4f6126594cba2f160c387d6662163893d
=Ali Alhashim <ali-alhashim@outlook.com>
2024-11-15 23:25:38 +0300

    Add EmployeeDto

--- src/main/java/com/alhashim/oneIT/controllers/EmployeeController.java
+++ src/main/java/com/alhashim/oneIT/controllers/EmployeeController.java
@@ -1,16 +1,26 @@
 package com.alhashim.oneIT.controllers;
 
 
+import com.alhashim.oneIT.dto.EmployeeDto;
 import com.alhashim.oneIT.models.Employee;
 import com.alhashim.oneIT.repositories.EmployeeRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -19,6 +29,9 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
 
     @GetMapping("/list")
@@ -34,7 +47,86 @@ public class EmployeeController {
     @GetMapping("/add")
     public String addEmployeePage(Model model)
     {
+        EmployeeDto employeeDto = new EmployeeDto();
+        model.addAttribute("employeeDto",employeeDto);
         return "employee/add";
+    }
+
+    @PostMapping("/add")
+    public String CreateEmployee(@Valid @ModelAttribute EmployeeDto employeeDto, BindingResult result)
+    {
+        String theNextBadgeNumber ="";
+        String storageFileName ="";
+
+
+
+        if(employeeDto.getBadgeNumber().isEmpty())
+        {
+            //generate auto badgeNumber
+            //get last employee badge number like A0000 + 1 the new = A0001
+        }
+
+
+        if(!employeeDto.getImageFile().isEmpty())
+        {
+            //Save image file
+            MultipartFile image = employeeDto.getImageFile();
+            Date createdAt = new Date();
+            storageFileName = createdAt.getTime()+"_"+ image.getOriginalFilename();
+            String uploadDir = "public/images/"+theNextBadgeNumber+"/";
+            Path uploadPath = Paths.get(uploadDir);
+
+
+           try
+           {
+               if(!Files.exists(uploadPath))
+               {
+                   Files.createDirectories(uploadPath);
+               }
+
+               InputStream inputStream = image.getInputStream();
+               Files.copy(inputStream, Paths.get(uploadDir + storageFileName), StandardCopyOption.REPLACE_EXISTING);
+
+           }
+           catch (Exception e)
+           {
+               System.out.println("saving image file exception "+ e.getMessage() );
+
+           }
+
+
+        }
+
+           Employee employee = new Employee();
+
+           employee.setBadgeNumber(employeeDto.getBadgeNumber());
+           employee.setName(employeeDto.getName());
+           employee.setCreatedAt(LocalDateTime.now());
+           employee.setPersonalMobile(employeeDto.getPersonalMobile());
+           employee.setAr_name(employeeDto.getAr_name());
+           employee.setPassword(passwordEncoder.encode(employeeDto.getPassword()));
+           employee.setBirthDate(employeeDto.getBirthDate());
+           employee.setGender(employeeDto.getGender());
+           employee.setImageFileName(storageFileName);
+           employee.setOfficeLocation(employeeDto.getOfficeLocation());
+           employee.setStatus(employeeDto.getStatus());
+           employee.setWorkMobile(employeeDto.getWorkMobile());
+           employee.setWorkEmail(employeeDto.getWorkEmail());
+
+
+           try
+           {
+               employeeRepository.save(employee);
+           }
+           catch (Exception e)
+           {
+               System.out.println(e.getMessage());
+           }
+
+
+
+            return "redirect:/employee/list";
+
     }
 
     @GetMapping("/detail")
---
+++ src/main/java/com/alhashim/oneIT/dto/EmployeeDto.java
@@ -0,0 +1,178 @@
+package com.alhashim.oneIT.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+public class EmployeeDto {
+
+    @NotEmpty(message = "The name is required")
+    private String name;
+
+    private String badgeNumber;
+
+    private String ar_name;
+
+    private String workEmail;
+
+    private String personalEmail;
+
+    private String officeLocation;
+
+    private MultipartFile imageFile;
+
+    private Date birthDate;
+
+    private String gender;
+
+    private String status;
+
+    @NotEmpty(message = "set password is required")
+    private String password;
+
+    private boolean is_USER;
+    private boolean is_MANAGER;
+    private boolean is_SUPPORT;
+    private boolean is_SUPERADMIN;
+
+
+    private String personalMobile;
+    private String workMobile;
+
+    public @NotEmpty(message = "The name is required") String getName() {
+        return name;
+    }
+
+    public void setName(@NotEmpty(message = "The name is required") String name) {
+        this.name = name;
+    }
+
+    public String getBadgeNumber() {
+        return badgeNumber;
+    }
+
+    public void setBadgeNumber(String badgeNumber) {
+        this.badgeNumber = badgeNumber;
+    }
+
+    public String getAr_name() {
+        return ar_name;
+    }
+
+    public void setAr_name(String ar_name) {
+        this.ar_name = ar_name;
+    }
+
+    public String getWorkEmail() {
+        return workEmail;
+    }
+
+    public void setWorkEmail(String workEmail) {
+        this.workEmail = workEmail;
+    }
+
+    public String getPersonalEmail() {
+        return personalEmail;
+    }
+
+    public void setPersonalEmail(String personalEmail) {
+        this.personalEmail = personalEmail;
+    }
+
+    public String getOfficeLocation() {
+        return officeLocation;
+    }
+
+    public void setOfficeLocation(String officeLocation) {
+        this.officeLocation = officeLocation;
+    }
+
+    public MultipartFile getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(MultipartFile imageFile) {
+        this.imageFile = imageFile;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public @NotEmpty(message = "set password is required") String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotEmpty(message = "set password is required") String password) {
+        this.password = password;
+    }
+
+    public boolean isIs_USER() {
+        return is_USER;
+    }
+
+    public void setIs_USER(boolean is_USER) {
+        this.is_USER = is_USER;
+    }
+
+    public boolean isIs_MANAGER() {
+        return is_MANAGER;
+    }
+
+    public void setIs_MANAGER(boolean is_MANAGER) {
+        this.is_MANAGER = is_MANAGER;
+    }
+
+    public boolean isIs_SUPPORT() {
+        return is_SUPPORT;
+    }
+
+    public void setIs_SUPPORT(boolean is_SUPPORT) {
+        this.is_SUPPORT = is_SUPPORT;
+    }
+
+    public boolean isIs_SUPERADMIN() {
+        return is_SUPERADMIN;
+    }
+
+    public void setIs_SUPERADMIN(boolean is_SUPERADMIN) {
+        this.is_SUPERADMIN = is_SUPERADMIN;
+    }
+
+    public String getPersonalMobile() {
+        return personalMobile;
+    }
+
+    public void setPersonalMobile(String personalMobile) {
+        this.personalMobile = personalMobile;
+    }
+
+    public String getWorkMobile() {
+        return workMobile;
+    }
+
+    public void setWorkMobile(String workMobile) {
+        this.workMobile = workMobile;
+    }
+}
